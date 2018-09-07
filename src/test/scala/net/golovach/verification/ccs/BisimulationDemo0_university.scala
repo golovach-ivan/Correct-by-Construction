30ff1d0274419f58681a4d52674668dc1bd5ea3f
golovach-ivan <golovach-ivan@users.noreply.github.com>
2018-09-07 17:11:21 +0300

    Update BisimulationDemo0_university.scala

--- src/test/scala/net/golovach/verification/ccs/BisimulationDemo0_university.scala
+++ src/test/scala/net/golovach/verification/ccs/BisimulationDemo0_university.scala
@@ -4,22 +4,26 @@ import net.golovach.verification.LTSLib._
 import net.golovach.verification.ccs.CCSLib._
 import net.golovach.verification.ccs.BisimulationLib._
 
+// CM ≡ Cofee Machine
+// CSh ≡ Computer Scientist (honest)
+// CSr ≡ Computer Scientist (real)
+// Univ ≡ University (implementation)
+// Spec ≡ Specification
 object BisimulationDemo0_university extends App {
 
   val $ = "$"
   val ☕ = "☕"
   val ✉ = "✉"
 
+  // === impl
   val CM = ↑($) :: ↓(☕) :: ⟲
   val CSh = ↓($) :: ↑(☕) :: ↓(✉) :: ⟲
   val CSr = (↓($) :: ↑(☕) :: ↓(✉) :: ⟲) + (↑(☕) :: ↓(✉) :: ⟲)
   val Univ = (CSh | CM | CSr) \ ($, ☕)
 
+  // === spec
   val Spec = ↓(✉) :: ⟲
 
   println(Univ ~ Spec)
   println(Univ ≈ Spec)
-
-  println(Univ.asLTS)
-  println(saturate(Univ.asLTS))
 }
